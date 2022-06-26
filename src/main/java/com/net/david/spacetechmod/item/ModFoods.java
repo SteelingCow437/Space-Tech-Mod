faf32e6176a92734f9bdb379cf406c10627c9c7c
SteelingCow437 <79171525+SteelingCow437@users.noreply.github.com>
2022-06-26 10:57:36 -0400

    Add a baguette to spite a 12 year old

--- src/main/java/com/net/david/spacetechmod/item/ModFoods.java
+++ src/main/java/com/net/david/spacetechmod/item/ModFoods.java
@@ -8,4 +8,8 @@ public class ModFoods {
     //Declare foodstuffs below this line!
     public static final FoodProperties LEAN = (new FoodProperties.Builder()).nutrition(8).saturationMod(2f).effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 2400, 1), 1F)
             .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400, 1), 1F).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400, 1), 1F).build();
+
+
+    public static final FoodProperties BAGUETTE = (new FoodProperties.Builder()).nutrition(15).saturation(10f).build();
+
 }
